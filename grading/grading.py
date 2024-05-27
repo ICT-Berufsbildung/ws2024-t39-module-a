b9784fc7aa921dcdcfb81caebda6905796df251d
thushjandan <thushjandan@gmail.com>
2024-05-27 22:10:09 +0200

    feat: add ldap port check.

--- grading/grading.py
+++ grading/grading.py
@@ -8,8 +8,7 @@ nr = InitNornir(
     inventory={"plugin": "SimpleInventory", "options": {"host_file": "inventory/hosts.yaml", "defaults_file": "inventory/defaults.yaml"}}
 )
 host_int_srv = nr.filter(name="int-srv01")
-
 results = host_int_srv.run(
     task=criterion_a1.task_A1_01
 )
-print_result(results)
+print_result(results, vars=["point"])
--- grading/tasks/criterion_a1.py
+++ grading/tasks/criterion_a1.py
@@ -1,11 +1,21 @@
 from nornir.core.task import Task, Result
 from nornir_paramiko.plugins.tasks import paramiko_command
+from nornir_paramiko.exceptions import CommandError
 
 def task_A1_01(task: Task) -> Result:
-    
-    task.run(task=paramiko_command, command="ip addr")
+    command = "nc -z -w3 localhost 389"
+    try:
+        task.run(task=paramiko_command, command=command)
+        # Exit code is 0
+        got_mark = True
+    except CommandError:
+        # Exit code is 1
+        got_mark = False
     """LDAP check"""
     return Result(
         host=task.host,
-        result=f"{task.host.name} says hello world!"
+        result=f"LDAP port tcp/389 is reachable" if got_mark else "LDAP port tcp/389 is NOT reachable",
+        command_run=command,
+        point=0.1 if got_mark else 0.0,
+        max_point=0.1
     )
